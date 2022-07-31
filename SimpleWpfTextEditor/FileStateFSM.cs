840ccc9f10838a24eeaba173116209a88129b796
YuriiRG <yuriirg@tutanota.com>
2022-07-31 18:24:18 +0200

    Create FSM for file states

---
+++ SimpleWpfTextEditor/FileStateFSM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWpfTextEditor
+{
+    // It's finite-state machine, explanation:
+    // https://en.wikipedia.org/wiki/Finite-state_machine
+    // https://stackoverflow.com/questions/5923767/
+    class FileStateFSM
+    {
+        public FileStates State { get; private set; } = FileStates.NoFile;
+
+        private readonly FileStates[,] fsm = new FileStates[3, 2]
+        {
+            // FileOpened,             FileChanged
+            {FileStates.FileNoChanges, FileStates.NoFile},      // NoFile
+            {FileStates.FileNoChanges, FileStates.ChangedFile}, // FileNoChanges
+            {FileStates.FileNoChanges, FileStates.ChangedFile}  // ChangedFile
+        };
+
+        public void EventHappened(FileEvents newEvent)
+        {
+            State = fsm[(int)State, (int)newEvent];
+        }
+    }
+    enum FileStates
+    {
+        NoFile,
+        FileNoChanges,
+        ChangedFile
+    }
+    enum FileEvents
+    {
+        FileOpened,
+        FileChanged
+    }
+}
