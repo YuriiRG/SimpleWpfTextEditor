78ecf486ca85c59f961b913a47ba7904704e42b4
YuriiRG <yuriirg@tutanota.com>
2022-08-16 14:28:25 +0200

    Translate some auto-generated summaries to English

--- SimpleWpfTextEditor/FontDialog.xaml.cs
+++ SimpleWpfTextEditor/FontDialog.xaml.cs
@@ -6,7 +6,7 @@ using System.Windows.Media;
 namespace SimpleWpfTextEditor
 {
     /// <summary>
-    /// Логика взаимодействия для FontDialog.xaml
+    /// Interaction logic for FontDialog.xaml
     /// </summary>
     public partial class FontDialog : Window
     {
--- SimpleWpfTextEditor/SearchDialog.xaml.cs
+++ SimpleWpfTextEditor/SearchDialog.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using SimpleWpfTextEditor.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SimpleWpfTextEditor
 {
     /// <summary>
-    /// Логика взаимодействия для SearchDialog.xaml
+    /// Interaction logic for SearchDialog.xaml
     /// </summary>
     public partial class SearchDialog : Window
     {
