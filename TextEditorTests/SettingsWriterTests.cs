5b3c94fa83d895304d8190cf5c52afffe0328ccb
YuriiRG <yuriirg@tutanota.com>
2022-08-17 17:09:18 +0200

    Write a test for SettingsWriter

---
+++ TextEditorTests/SettingsWriterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TextEditorTests
+{
+    [TestClass]
+    public class SettingsWriterTests
+    {
+        [TestMethod]
+        public void WriteReadSettings()
+        {
+            AppSettings sampleSettings = new()
+            {
+                RecentFiles =
+                {
+                    "D:\\mytemp\\visualstudio\\newlinetest\\LF.txt",
+                    "D:\\mytemp\\visualstudio\\test4.txt",
+                    "D:\\mytemp\\visualstudio\\newlinetest\\CRLF.txt"
+                },
+                FontFamily = "Wingdings",
+                FontSize = 18,
+                WrapText = false,
+                Locale = "ru-RU"
+            };
+
+            SettingsWriter.Save(sampleSettings);
+            var actualSettings = SettingsWriter.Read();
+
+            Assert.IsTrue(Enumerable.SequenceEqual(sampleSettings.RecentFiles, actualSettings.RecentFiles));
+            Assert.AreEqual(sampleSettings.FontFamily, actualSettings.FontFamily);
+            Assert.AreEqual(sampleSettings.FontSize, actualSettings.FontSize);
+            Assert.AreEqual(sampleSettings.WrapText, actualSettings.WrapText);
+            Assert.AreEqual(sampleSettings.Locale, actualSettings.Locale);
+
+        }
+    }
+}
--- TextEditorTests/Usings.cs
+++ TextEditorTests/Usings.cs
@@ -2,3 +2,4 @@ global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using SimpleWpfTextEditor;
 global using SimpleWpfTextEditor.Data;
 global using SimpleWpfTextEditor.Helpers;
+global using Moq;
