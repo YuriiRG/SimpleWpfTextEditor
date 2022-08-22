5274b7fec4e0f947890c55504e82bf3c0a76cb08
YuriiRG <yuriirg@tutanota.com>
2022-08-22 14:32:05 +0200

    Write tests for the FSM

---
+++ TextEditorTests/FileStateFSMTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditorTests
+{
+    [TestClass]
+    public class FileStateFSMTests
+    {
+        [TestMethod]
+        public void TypicalUsage()
+        {
+            FileStateFSM stateMachine = new();
+            
+            Assert.AreEqual(stateMachine.State, FileStates.NoFile);
+            
+            stateMachine.EventHappened(FileEvents.FileChanged);
+
+            Assert.AreEqual(stateMachine.State, FileStates.NoFile);
+
+            stateMachine.EventHappened(FileEvents.FileSaved);
+
+            Assert.AreEqual(stateMachine.State, FileStates.FileNoChanges);
+
+            stateMachine.EventHappened(FileEvents.FileChanged);
+
+            Assert.AreEqual(stateMachine.State, FileStates.ChangedFile);
+
+            stateMachine.EventHappened(FileEvents.FileSaved);
+
+            Assert.AreEqual(stateMachine.State, FileStates.FileNoChanges);
+
+            stateMachine.EventHappened(FileEvents.FileOpened);
+
+            Assert.AreEqual(stateMachine.State, FileStates.FileNoChanges);
+
+            stateMachine.EventHappened(FileEvents.FileChanged);
+
+            Assert.AreEqual(stateMachine.State, FileStates.ChangedFile);
+
+            stateMachine.EventHappened(FileEvents.FileSaved);
+
+            Assert.AreEqual(stateMachine.State, FileStates.FileNoChanges);
+        }
+    }
+}
